6c7ce19704b88d02cfc5f831dca2fa431fd14e76
Andy Fang <andy.fang@airbnb.com>
2018-11-03 19:20:03 -0700

    global writer with spinlocks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+extern crate lazy_static;
 extern crate bootloader_precompiled;
+extern crate volatile;
+extern crate spin;
+
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -11,17 +17,10 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
     loop {}
 }
